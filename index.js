a0263406cd0948baa6ac6729b76f549690516e0a
unknown <gakutyn.2020@gmail.com>
2021-07-03 09:11:51 +0900

    バージョン対策

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ module.exports = (text = String(), cb = Function()) => {
             .then(res => {
                 if (res.status == 200) {
                     res.json().then(json => {
-                        if (typeof cb == "function") {
-                            cb(json);
-                            resolve(json);
-                        }else{
-                            resolve(json);
-                        };
+                        if (json.version.startsWith("1")) {
+                            if (typeof cb == "function") {
+                                cb(json);
+                                resolve(json);
+                            } else {
+                                resolve(json);
+                            };
+                        } else {
+                            reject(`バージョンエラー: 互換性のないLainanAPIバージョンが指定されました。(${json.version})`);
+                        }
                     });
                 } else {
                     reject(`リクエストエラー: LainanAPIから返されたステータスが200ではありません。(${res.status})`);
